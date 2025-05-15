1907079b029ec80a48aa4a3d618ec30ae0374f7e
MohsinKhan <mohsinkhan.0787@gmail.com>
2025-05-15 14:34:20 +0500

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mohsin Khan on 15/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
